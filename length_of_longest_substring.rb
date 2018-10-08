56ae929f4b9bf55b314ec1326c74c574c2b22774
huyphung1602 <huy.phung.sw@gmail.com>
2018-10-09 01:05:32 +0700

    Add optimize 1 loop for length of longest substring

--- length_of_longest_substring.rb
+++ length_of_longest_substring.rb
@@ -20,20 +20,42 @@
 # @param {String} s
 # @return {Integer}
 
-#Two loops, O(n), use hash
+# #Two loops, O(n), use hash
+# def length_of_longest_substring(s)
+#   exist = {}
+#   max_length = 0
+#   i = 0
+
+#   s.chars.each_with_index do |char, j|
+#     while exist[char]
+#       exist[s[i]] = false
+#       i += 1
+#     end
+
+#     exist[char] = true
+#     max_length = [j - i + 1, max_length].max
+#   end
+
+#   max_length
+# end
+
+# s = 'abcabcbb'
+# puts length_of_longest_substring(s)
+
+
+# Optimize O(n) runtime, O(1) space, 1 loop
 def length_of_longest_substring(s)
-  exist = {}
+  char_map = {}
   max_length = 0
   i = 0
 
   s.chars.each_with_index do |char, j|
-    while exist[s[j]]
-      exist[s[i]] = false
-      i += 1
+    if char_map[char] && char_map[char] >= i
+      i = char_map[char] + 1
     end
 
-    exist[char] = true
-    max_length = [j-i+1, max_length].max
+    char_map[char] = j
+    max_length = [j - i + 1, max_length].max
   end
 
   max_length
